7420eceaf5ab913acb3be08c7ef7893fc2339752
500apps-jaspreet <85924850+500apps-jaspreet@users.noreply.github.com>
2023-04-19 01:03:26 +0530

    Update apna.py

--- sample/spiders/apna.py
+++ sample/spiders/apna.py
@@ -2,29 +2,25 @@ import scrapy
 
 class ApnaSpider(scrapy.Spider):
     name = 'apna'
-    allowed_domains = ['https://apna.co/jobs']
+    allowed_domains = ['apna.co']
     start_urls = ['https://apna.co/jobs']
-
 
     custom_settings = {
         'FEED_FORMAT': 'json',
         'FEED_URI': 'jobs.json'
     }
 
-
     def parse(self, response):
-        titles = response.xpath("//div[@class='JobCardList__Grid-sc-1v9ot9b-1 heAxPY']/div/div/div/h3/a/text()").getall()
-        # company_name = response.xpath(" //p[@class='styles__JobOrganizationName-sc-1eqgvmq-6 hqjYPR']").getall()
+        jobs = response.xpath("//div[@class='JobCardList__Grid-sc-1v9ot9b-1 heAxPY']/div")
+        for job in jobs:
+            title = job.xpath(".//div[@class='JobCard__JobHeading-sc-1j63e6z-0 jZWHjc']/h3/a/text()").get()
+            location = job.xpath(".//div[@class='JobCard__JobDescription-sc-1j63e6z-1 bzkSml']/p[1]/text()").get()
+            company = job.xpath(".//span[contains(@class, 'styles__JobOrganizationName')]/text()").get()
+            salary = job.xpath(".//p[contains(text(), '₹')]/text()").get()
 
-
-        for title in titles:
-
-        #      yield {'title': title.strip(),'companyname':company_name.strip()}
-        #      yield(title, company_name)
-            yield {'title': title.strip()}
-            yield(title)
-
-        print(response)
-        ############################
-
-
+            yield {
+                'title': title.strip(),
+                'location': location.strip(),
+                'company': company.strip(),
+                'salary': salary.strip(),
+            }
